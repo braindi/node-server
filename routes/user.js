c9bf578bcef3f8f20825d651ae7bc19b51a459d7
braindi <150654954+braindi@users.noreply.github.com>
2024-03-02 22:54:45 +0200

    post

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,6 @@ import { authAdmin } from "../middlewares/auth.js";
 const router = express.Router();
 router.get("/", authAdmin , userController.getAllUsers);
 router.post("/", userController.addUser);
-router.get("/login/signIn", userController.login);
+router.post("/login/signIn", userController.login);
 
 export default router;
