3ceaae425656cbd044b1892de06b8189aae5c106
braindi <150654954+braindi@users.noreply.github.com>
2024-03-03 19:34:23 +0200

    Update bag.js4

--- models/bag.js
+++ models/bag.js
@@ -3,12 +3,12 @@ import Joi from "joi";
 
 const bagSchema = mongoose.Schema({
     company: String,
-    description: { type: String, default: "" },
+    description: { type: String, default: " " },
     color: String,
     price: Number,
     size: { type: String, default: "M" },
-    width: { type: Number, default : "" },
-    height: { type: Number, default : "" },
+    width: { type: Number, default : 0 },
+    height: { type: Number, default : 0 },
     generateDate: { type: Date, default: Date.now() },
     imgUrl: { type: String, default: "_1_8_18465_1.jpg"}
    
@@ -19,12 +19,12 @@ export const Bag = mongoose.model("bags", bagSchema);
 export const bagValidator = (bagToValidate) => {
     let bagJoi = Joi.object({
         company: Joi.string().min(2).max(15).required(),
-        description: Joi.string().default(""),
+        description: Joi.string().default(" "),
         color: Joi.string().required(),
         price: Joi.number().required(),
         size: Joi.string().default("M"),
-        width: Joi.number().default(""),
-        height: Joi.number().default(""),
+        width: Joi.number().default(0),
+        height: Joi.number().default(0),
         generateDate: Joi.date().default(Date.now()),
         imgUrl:Joi.string().default('_1_8_18465_1.jpg')
     }).unknown()
